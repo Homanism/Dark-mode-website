836106eb223519b4398f900523554458d6a254fb
Homan Mousavi <homanm@ulrik.uio.no>
2023-03-18 01:25:56 +0100

    fix: design

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ if (currentTheme) {
 
 // Create a new instance of Typed.js
 var typed = new Typed('#typed', {
-    strings: ['full-stack developer.', 'Utvikler.', 'Designer.'],
+    strings: ['full-stack <br/> developer.', 'Utvikler.', 'Designer.'],
     typeSpeed: 30,
     backSpeed: 20,
     loop: true,
